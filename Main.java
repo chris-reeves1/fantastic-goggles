0a808c9a1179fc6f8094470fc94f73c5cd0dbebf
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2026-01-15 14:57:35 +0000

    Implement CreateTable class for customer table creation
    Added a CreateTable class to create a customers table in an H2 database.

--- Main.java
+++ Main.java
@@ -254,3 +254,28 @@ Why use?
     </dependencies>
 </project>
 
+
+    package abstractsInterfaces;
+import java.sql.*;
+
+public class CreateTable {
+    private static final String URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
+    private static final String USER = "sa";
+    private static final String PASS = ""; 
+    public static void main(String[] args) throws Exception{
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASS)){
+            Statement st = conn.createStatement();{
+                String ddl = """
+                        CREATE TABLE IF NOT EXISTS customers (
+                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        name VARCHAR(100) NOT NULL,
+                        city VARCHAR(100) NOT NULL 
+                        )
+                        """;
+                        st.execute(ddl);
+                        System.out.println("Table created successfully.");
+            }
+    }
+}
+}
+
